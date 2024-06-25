dda2b186b9c3f0c97de59c058217a745008f3afb
heyolaniran <olaniran.abd@gmail.com>
2024-06-25 11:52:24 +0100

    feat: guide section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/Hero";
 import Camp from "@/components/Camp";
-
+import Guide from "@/components/Guide";
 export default function Home() {
   return (
     <>
       <Hero/>
       <Camp />
+      <Guide />
     </>
   );
 }
