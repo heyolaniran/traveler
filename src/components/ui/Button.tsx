60bc828dc1ee7b8e04ea25ec9a3c17c349c099ef
heyolaniran <olaniran.abd@gmail.com>
2024-06-26 10:16:23 +0100

    feat: cta section

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,9 +1,9 @@
 
-export default function Button ({title, type, icon, position , variant , otherClasses} : {title : string , type : "submit" | "reset" | "button" , icon : React.ReactNode, position : 'right' | 'left' , variant? : 'btn_dark_green' | string, otherClasses? : string}) {
+export default function Button ({title, type, icon, position , variant , otherClasses, full} : {title : string , type : "submit" | "reset" | "button" , icon : React.ReactNode, position : 'right' | 'left' , variant? : 'btn_dark_green' | string, otherClasses? : string, full? : boolean}) {
 
     return (
 
-        <button type={type} className={` text-sm ${otherClasses} flexCenter gap-3  rounded-full border ${variant} font-semibold `} >
+        <button type={type} className={` text-sm ${otherClasses} flexCenter gap-3 ${full && 'w-full'}  rounded-full border ${variant} font-semibold `} >
          {position === 'left' && icon}
 
          {title} 
